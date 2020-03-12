58a3974e9e183cccf25280adf5978bbee9fad407
ayan456 <ayanbiswas184@gmail.com>
2020-03-12 22:56:10 +0530

    first files modified

--- Beginner/getting_started.py
+++ Beginner/getting_started.py
@@ -1,32 +1,29 @@
+'''Beginning Program'''
+
 import cv2
 
-cap = cv2.VideoCapture(0)
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi', fourcc,20.0,(640,480))
+CAP = cv2.VideoCapture(0)
+FOURCC = cv2.VideoWriter_fourcc(*'XVID')
+OUT = cv2.VideoWriter('output.avi', FOURCC, 20.0, (640, 480))
 
 
 
-print(cap.isOpened())
-while(cap.isOpened()):
-    ret,frame = cap.read()
-    if ret ==True:
+print(CAP.isOpened())
+while CAP.isOpened():
+    RET, FRAME = CAP.read()
+    if RET:
         #print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         #cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        
         #cap.get(cv2.CAP_PROP_FRAME_COUNT)
-        out.write(frame)
+        OUT.write(FRAME)
 
-        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        cv2.imshow('Show Your Face', gray)
+        GRAY = cv2.cvtColor(FRAME, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('Show Your Face', GRAY)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
         break
 
-cap.release()
-out.release()
+CAP.release()
+OUT.release()
 cv2.destroyAllWindows()
-
-
-
-

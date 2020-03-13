a0d6a64552bb27d406cdbe8b47ad12f4483d354a
ayan456 <ayanbiswas184@gmail.com>
2020-03-13 12:53:30 +0530

    add text on video added

---
+++ Beginner/add_text_to_video.py
@@ -0,0 +1,28 @@
+'''Basic video Recorder  Program'''
+import cv2
+
+CAP = cv2.VideoCapture(0)
+
+print(CAP.get(cv2.CAP_PROP_FRAME_WIDTH))
+print(CAP.get(cv2.CAP_PROP_FRAME_HEIGHT))
+print(CAP.get(cv2.CAP_PROP_FRAME_COUNT))
+
+CAP.set(3, 1208)
+CAP.set(3, 720)
+
+print(CAP.get(3))
+print(CAP.get(4))
+
+while CAP.isOpened():
+    RET, FRAME = CAP.read()
+    if RET:
+
+        GRAY = cv2.cvtColor(FRAME, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('YOUR FACE', GRAY)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+CAP.release()
+cv2.destroyAllWindows()

a2e5a52e7eb082c08d291a7560e8ce6921e71fd0
ayan456 <ayanbiswas184@gmail.com>
2020-03-14 21:24:03 +0530

    minor changes in last commit

--- Beginner/face_detect_fromvideo.py
+++ Beginner/face_detect_fromvideo.py
@@ -7,19 +7,14 @@ VIDEO_CAPTURE = cv2.VideoCapture(0)
 
 while True:
     _, IMG = VIDEO_CAPTURE.read()
-    GRAY_IMG = cv2.cvtColor(IMG, cv2.COLOR_BGR2GRAY)
-    FACES = FACE_CASCADE.detectMultiScale(GRAY_IMG, scaleFactor=1.05, minNeighbors=4)
-    #cv2.imshow('Face detection', GRAY_IMG)
-    #cv2.imshow('Face detection', IMG)
+    FACES = FACE_CASCADE.detectMultiScale(IMG, scaleFactor=1.05, minNeighbors=4)
     for x, y, w, h in FACES:
         IMG = cv2.rectangle(IMG, (x, y), (x+w, y+h), (0, 255, 0), 4)
-        cv2.imshow('Face', IMG)
+        FLIP_IMAGE = cv2.flip(IMG, 1)
+        cv2.imshow('Face', FLIP_IMAGE)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     
-
-
-
 VIDEO_CAPTURE.release()
 cv2.destroyAllWindows()
 
